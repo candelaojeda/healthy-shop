eaa2370e95ba41ed1a3bcfff30499359153a638d
candelaojeda <ojedacandelaco@gmail.com>
2022-02-02 11:21:44 -0300

    added functions with events

---
+++ js/main.js
@@ -0,0 +1,60 @@
+// VARIABLES
+let nav = document.getElementById("nav");
+let menu = document.getElementById("enlaces");
+let abrir = document.getElementById("open");
+let botones = document.getElementsByClassName("btn-header");
+let cerrado = true;
+
+function menus() {
+  let desplazamiento_actual = window.pageYOffset;
+
+  if (desplazamiento_actual <= 200) {
+    nav.classList.remove("nav2");
+    nav.className = "nav1";
+    nav.style.transition = "1s";
+    menu.style.top = "70px";
+  } else {
+    nav.classList.remove("nav1");
+    nav.className = "nav2";
+    menu.style.top = "75px";
+  }
+}
+function apertura() {
+  if (cerrado) {
+    menu.style.width = "70vw";
+    cerrado = false;
+  } else {
+    menu.style.width = "0%";
+    menu.style.overflow = "hidden";
+    cerrado = true;
+  }
+}
+
+window.addEventListener("load", function () {
+  $("#onload").fadeOut();
+  $("body").removeClass("hidden");
+  menus();
+});
+window.addEventListener("click", function (e) {
+  if (cerrado == false) {
+    let span = document.querySelector("span");
+    if (e.target !== span && e.target !== abrir) {
+      menu.style.width = "0%";
+      menu.style.overflow = "hidden";
+      cerrado = true;
+    }
+  }
+});
+window.addEventListener("scroll", function () {
+  menus();
+});
+window.addEventListener("resize", function () {
+  if (screen.width >= 700) {
+    cerrado = true;
+    menu.style.removeProperty("overflow");
+    menu.style.removeProperty("width");
+  }
+});
+abrir.addEventListener("click", function () {
+  apertura();
+});
